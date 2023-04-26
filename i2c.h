bd1f0c4ddcf43e67cae4c50c75777f2cd8bdbf9b
root <manolkata4@abv.bg>
2023-04-26 21:42:27 +0300

    make functions public

--- i2c.h
+++ i2c.h
@@ -3,10 +3,9 @@ class I2C
         private:
                 int file;
 
+        public:
                 int openFile(unsigned char address);
                 int closeFile();
-
-        public:
                 int sendFile(char *buffer, int num);
                 int readFile(unsigned char *buffer, int num);
 
