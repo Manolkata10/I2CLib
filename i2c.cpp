cbe8d281aeef5497844c6bd78515fb11d1a969f6
root <manolkata4@abv.bg>
2023-04-26 20:27:21 +0300

    remove wrongly copied clas

--- i2c.cpp
+++ i2c.cpp
@@ -57,20 +57,4 @@ I2C::~I2C()
 {                                                                               
         closeFile();                                                            
 }                                                                               
-                                                                                
-root@a13-olinuxino:~/ivan# cat i2c.h                                            
-class I2C                                                                       
-{                                                                               
-        private:                                                                
-                int file;                                                       
-                                                                                
-                int openFile(unsigned char address);                            
-                int closeFile();                                                
-                                                                                
-        public:                                                                 
-                int sendFile(char *buffer, int num);                            
-                int readFile(unsigned char *buffer, int num);                   
-                                                                                
-                I2C();                                                          
-                ~I2C();                                                         
-};    
+  
